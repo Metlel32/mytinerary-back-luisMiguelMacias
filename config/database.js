11838c9ca53f6310d6a12f77ef1c24dd52e500ae
Luis Macias <metlel32@gmail.com>
2025-03-20 09:13:59 -0500

    instalacion de dependecencias morgan y funciones del servidor todas listas

--- config/database.js
+++ config/database.js
@@ -1,4 +1,6 @@
-let url = ""
+import mongoose from "mongoose"
+
+let url = process.env.URI_MONGO
 mongoose.connect(url)
 .then(()=> console.log("database connectd"))
 .catch(error => console.log(error))
--- server.js
+++ server.js
@@ -1,11 +1,23 @@
 import express from "express"
 import "dotenv/config.js"
+import './config/database.js'
+import cors from 'cors'
+import morgan from "morgan"
+
+
 
 const PORT = process.env.PORT || 8080
 const server = express()
 const redy = () => console.log("working: " + PORT);
 
 
+//usar cors y morgan
+server.use(express.urlencoded({extended:true}))
+server.use(express.json())
+server.use(cors())
+server.use(morgan('dev'))
 
 
-server.listen(PORT, redy)
+
+//al agregar una ip local puedo ver en cualquier dispositivo de mi red
+server.listen(PORT, "0.0.0.0", redy)
